a3f69a2ac38271d16cd49a12f3c01a4659f0f90a
MANAV13899 <46744841+MANAV13899@users.noreply.github.com>
2019-09-22 00:21:07 +0530

    scrape photos out of instagram

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# intascraper
- 
+# Instacraper
+
+> Scrap images from an instagram url and save them locally
+
+## Installation
+
+```sh
+npm install -g instacraper
+```
+
+## Usage
+
+```sh
+instacraper 'https://www.instagram.com/p/Bx3F46OFTju/'
+```
+
+Show help with `-h`
+
+```sh
+instacraper -h
+```
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT)
---
+++ bin/cli.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs')
+const { Downloader } = require('../lib')
+
+const argv = yargs
+  .command(
+    '*',
+    'Scrap pictures from Instagram',
+    () => {},
+    argv => {
+      if (argv._.length) argv.url = argv._.shift()
+      if (!argv.url) {
+        yargs.showHelp()
+        process.exit(0)
+      }
+    }
+  )
+  .usage('Usage: $0 <file> [options]')
+  .example(
+    '$0 "https://www.instagram.com/p/Bxn1b1kHGVt"',
+    'Download all images from post'
+  )
+  .option('dest', {
+    describe: 'Folder to put the files',
+    alias: ['d', 'o', 'dist'],
+    default: 'images',
+  })
+  .help('h')
+  .alias('h', 'help')
+  .epilog('copyright 2019').argv
+
+async function main() {
+  const { url, dest } = argv
+
+  const insta = new Downloader({ url, dest })
+
+  await insta.start()
+  try {
+    const imgs = await insta.scrapImages()
+    await insta.downloadImages(imgs)
+  } catch (err) {
+    consola.error('Failed :(')
+  }
+  await insta.stop()
+}
+
+main()
